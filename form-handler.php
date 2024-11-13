9434b814765080bde7e0ea8fcc708201a392cdb1
Okon Emmanuel <112129238+Emani23paulo@users.noreply.github.com>
2024-11-13 14:15:10 +0100

    form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -13,7 +13,7 @@ $email_body = "User Name: $name.\n".
                 "Subject: $subject.\n".
                  "Message: $message.\n";
                  
-$to = 'info@westcoastlib.com';
+$to = 'legendaryemani07@gmail.com';
 
 $headers = "From: $email_from" \r\n";
 
